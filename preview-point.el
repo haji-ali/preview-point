b3e479cf123050838d23aa034bb375f4f7cc8313
Al Haji-Ali <a.hajiali@hw.ac.uk>
2025-08-28 12:26:41 +0200

    Added licence text

--- preview-point.el
+++ preview-point.el
@@ -6,6 +6,20 @@
 ;; Keywords: tex, preview, convenience
 ;; URL: https://github.com/hajiali/preview-point
 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 
 ;; This package provides an alternative preview mechanism for AUCTeX's
@@ -403,6 +417,7 @@ ORIG-FUN is the original function.  ARGS are its arguments."
                 (setq preview-abort-flag t)))
             (with-local-quit (accept-process-output cur-process))
             (preview-point--preview-at-point pt buffer))
+        ;; The code below is adopted from preview-auto
         (let ((TeX-suppress-compilation-message t)
               (save-silently t))
           (advice-add 'write-region :around
